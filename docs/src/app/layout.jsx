4b13cc1937cab687315b8a7853b7606b167bbfb9
shunquan.wang <shunquan.wang@ly.com>
2025-04-10 21:09:58 +0800

    fix: fix css level error

--- docs/src/app/layout.jsx
+++ docs/src/app/layout.jsx
@@ -2,7 +2,6 @@
 import { Head } from 'nextra/components';
 import { getPageMap } from 'nextra/page-map';
 import { Footer, Layout, Navbar } from 'nextra-theme-docs';
-import 'nextra-theme-docs/style.css';
 import './global.css';
 
 export const metadata = {
